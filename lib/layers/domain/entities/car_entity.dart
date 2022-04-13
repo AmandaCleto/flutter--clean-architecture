c51ea0dbd8f54d29d60e19f23325c4631f517308
Amanda Cleto <amanda.clet@gmail.com>
2022-04-13 20:18:22 -0300

    created entity car

---
+++ lib/layers/domain/entities/car_entity.dart
@@ -0,0 +1,15 @@
+class CarEntity {
+  String licensePlate;
+  int amountOfDoors;
+  double price;
+
+  CarEntity({
+    required this.licensePlate,
+    required this.amountOfDoors,
+    required this.price,
+  });
+
+  //business rule
+  //the car's price is calculated based on the amount of its doors
+  double get finalPrice => amountOfDoors * price;
+}
